69cabe06e0b5836d47b04ff1e618a90a5754f2d3
Avantika Tagde <avantikacybersec@gmail.com>
2026-05-31 17:23:23 +0530

    Added CORS support

--- demo/src/main/java/com/stt/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/stt/demo/config/SecurityConfig.java
@@ -22,11 +22,11 @@ public class SecurityConfig {
     private JwtFilter jwtFilter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
             throws Exception {
 
         http
+                .cors(cors -> {})
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
